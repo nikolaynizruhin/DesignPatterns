f6e21c488dfb4e75d0b264f0d83559ebe77eaec1
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2016-06-20 23:20:10 +0300

    add tests and change autoload

---
+++ tests/TextTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use phpunit\framework\TestCase;
+
+class TextTest extends TestCase
+{
+	/**
+	 * Set Up Text Object
+	 */
+	public function setUp()
+	{
+		$this->text = new Text;
+	}
+
+	/**
+	 * Test Content Length
+	 */
+    public function testContentLength()
+    {
+		$this->assertEquals(9, $this->text->getLength());
+    }
+
+    /**
+     * Test Adding Prefix and Postfixs
+     */
+    public function testAddPrefixAndPostfix()
+    {
+    	$this->text->addPrefixAndPostfix('prefix', 'postfix');
+    	$this->assertEquals(22, $this->text->getLength());
+    }
+}
